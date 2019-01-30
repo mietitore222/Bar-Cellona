a199d1ed4051ecb16bcb59755f726ac03544701b
Your Name <you@example.com>
2019-01-30 13:14:42 +0100

    fatto

--- code/app.js
+++ code/app.js
@@ -1,1 +1,13 @@
-allert("benvenuti nella casa blu!!!!");
+var app= {
+    init: function(){
+        //alert("benvenuti nella casa blu!!!!");
+        $.get("https://api.github.com/users/Matteo80300")
+        .done(app.onSuccess);
+    },
+    onSuccess: function(mydata){
+        console.log(mydata);
+    }
+};
+
+$(document).ready(app.init);
+
